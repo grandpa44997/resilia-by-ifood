e6fb6bdfe15f28a7daae5d4e8281343e1f7a458e
Daniel Freitas <bikemessengerateu@gmail.com>
2023-04-07 16:43:03 -0300

    arquivos de dois exercicios e um print da aula
    aprendemos sobre input e operações básicas

---
+++ exerciciomentoria7abr23.py
@@ -0,0 +1,8 @@
+numero1 = 10
+numero2 = 20
+
+#jeito do deynson
+
+print("soma", numero1 + numero2)
+print(f"soma é igual a {numero1 + numero2}")
+print(f"minha idade é: {10 + 10}")
---
+++ exerciciomentoriainput7abr23.py
@@ -0,0 +1,6 @@
+numero1 = int(input("qual o numero 1?"))
+numero2 = int(input("e o numero 2?"))
+
+soma = numero1 + numero2
+
+print(soma)

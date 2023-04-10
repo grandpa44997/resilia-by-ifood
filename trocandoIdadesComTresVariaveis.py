eb99a1616c84f50dcbf33b63d18c73c7facf1057
Daniel Freitas <bikemessengerateu@gmail.com>
2023-04-10 16:27:29 -0300

    exemplo do rafa
    python receber e trocar duas idades, o exemplo do professor baseado na analogia de potes usou um terceiro pote

---
+++ trocandoIdadesComTresVariaveis.py
@@ -0,0 +1,25 @@
+IdadePrim = 36
+IdadeSeg = 22
+
+print(IdadePrim)
+print(IdadeSeg)
+
+IdadeAux = IdadePrim
+IdadePrim = IdadeSeg
+IdadeSeg = IdadeAux
+
+print(IdadePrim)
+print(IdadeSeg)
+
+
+
+'''
+a = primeiro
+b = segundo
+c = auxiliar
+
+c = a
+a = b
+b = c
+
+'''
